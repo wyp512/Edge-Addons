ac380ad5a0a1a0e59f955da34669dde38e22e01c
wyp512 <wyp1137667201@gmail.com>
2025-03-25 23:58:07 +0800

    Update temp.py

--- Edge-Addons/icons/temp.py
+++ Edge-Addons/icons/temp.py
@@ -2,9 +2,9 @@ from PIL import Image
 
 
 
-original_image = Image.open('#')
+original_image = Image.open('./icons/Robot48.png')
 
 
 resized_image = original_image.resize((16, 16))
 
-resized_image.save('#')
+resized_image.save('Robot16.png')
